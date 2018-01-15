2301da64519d0b24da9472dad9ee63d7381bbd06
chenxiaocai <1561992298@qq.com>
2018-01-15 15:19:16 +0800

    dev

--- test.js
+++ test.js
@@ -3,8 +3,14 @@ function setName(){
     console.log("dev分支中的setName");
     console.log('0sdfsdffffffffffffffffffffff');
     console.log('0sdfsdffffffffffffffffffffff');
+    console.log('0sdfsdffffffff2423');
 }
 
 function test(){
     console.log('asdfa0sdf0');
+    console.log('0fgd0fg0d0f');
+}
+
+function test01 (){
+    console.log("sdfas2f12sd1212");
 }
