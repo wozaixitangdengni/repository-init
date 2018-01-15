bce65b2feb9ba8ab39f847697861b20c3b72ff90
chenxiaocai <1561992298@qq.com>
2018-01-15 15:23:14 +0800

    dev

--- test.js
+++ test.js
@@ -1,14 +1,14 @@
+function test(){
+    console.log('asdfa0sdf0');
+    console.log('0fgd0fg0d0f');
+}
+
 function setName(){
     console.log("setName");
     console.log("dev分支中的setName");
     console.log('0sdfsdffffffffffffffffffffff');
     console.log('0sdfsdffffffffffffffffffffff');
     console.log('0sdfsdffffffff2423');
-}
-
-function test(){
-    console.log('asdfa0sdf0');
-    console.log('0fgd0fg0d0f');
 }
 
 function test01 (){
